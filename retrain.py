c750b4e65b2e9685646ffee1fa3a542c2e971010
Rishit220106 <rishitdhagai220106@gmail.com>
2025-11-14 19:43:22 +0000

    Fix PyCaret 2.x setup args

--- retrain.py
+++ retrain.py
@@ -4,12 +4,13 @@ from pycaret.datasets import get_data
 # Load dataset
 iris = get_data('iris')
 
-# Setup PyCaret
-exp = setup(iris, target='species', session_id=42, silent=True, html=False)
+# Setup PyCaret (PyCaret 2.x compatible)
+exp = setup(iris, target='species', session_id=42, silent=True)
+# NOTE: remove html=False
 
 # Train & finalize model
 model = compare_models()
 final = finalize_model(model)
 
 # Save model
-save_model(final, 'iris_model')  # creates iris_model.pkl
+save_model(final, 'iris_model')
